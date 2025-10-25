523d30232c574b9e8eccfa6a05daca52b6528443
Daniel Gatis <danielgatis@gmail.com>
2025-10-24 22:09:32 -0300

    refactor CLI entry point to rename _main to main and streamline command registration

--- rembg/cli.py
+++ rembg/cli.py
@@ -6,11 +6,9 @@ from .commands import command_functions
 
 @click.group()
 @click.version_option(version=__version__)
-def _main() -> None:
+def main() -> None:
     pass
 
 
 for command in command_functions:
-    _main.add_command(command)
-
-_main()
+    main.add_command(command)
